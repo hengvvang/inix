e7a7e60729dc2a621b7df8c77bc952e0eb740ea8
hengvvang <hengvvang@proton.me>
2025-07-06 19:33:56 +0800

    ------------------ This version can work, but there are many features that are not tested

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -8,7 +8,30 @@
       ../../system
     ];
 
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    powerManagement.finegrained = false;
+    open = false;               # 使用闭源驱动
+    nvidiaSettings = true;      # 安装 nvidia-settings 图形工具
+  };
 
+  # 音频配置
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # 触摸板支持
+  services.libinput.enable = true;
+
+  # 打印支持
+  services.printing.enable = true;
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
