2f0e7cd33fb52309011bb54df771c8206996c1a0
hengvvang <hengvvang@proton.me>
2025-07-07 09:51:17 +0800

    home-manager 1.1.1

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
+      url = "github:hengvvang/zen-browser";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
