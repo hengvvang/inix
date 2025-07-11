5a12ebc138573d251e0ece7edf4c8ecf19f28355
hengvvang <hengvvang@gmail.com>
2025-07-11 22:02:56 +0800

    none

--- system/desktop/cosmic.nix
+++ system/desktop/cosmic.nix
@@ -5,21 +5,32 @@
   config = lib.mkIf (config.mySystem.desktop.enable && config.mySystem.desktop.preset == "cosmic") {
     # ----- cosmic -----
     services = {
-          displayManager.cosmic-greeter.package = pkgs.cosmic-greeter;
-          displayManager.cosmic-greeter.enable = true;
-          desktopManager.cosmic.enable = true;
-          desktopManager.cosmic.xwayland.enable = true;
-    };
-  # environment.cosmic.excludePackages = [
-  #   plasma-browser-integration # Comment out this line if you use KDE Connect
-  #   kdepim-runtime # Unneeded if you use Thunderbird, etc.
-  #   konsole # Comment out this line if you use KDE's default terminal app
-  #   oxygen
-  # ];
+      displayManager.cosmic-greeter.package = pkgs.cosmic-greeter;
+      displayManager.cosmic-greeter.enable = true;
+      desktopManager.cosmic.enable = true;
+      desktopManager.cosmic.xwayland.enable = true;
+  };
+    environment.cosmic.excludePackages = [
+      # plasma-browser-integration # Comment out this line if you use KDE Connect
+      # kdepim-runtime # Unneeded if you use Thunderbird, etc.
+      # konsole # Comment out this line if you use KDE's default terminal app
+      # oxygen
+    ];
 
 
 
     environment.systemPackages = [
+      # pkgs.cosmic-shell,
+      # pkgs.cosmic-terminal,
+      # pkgs.cosmic-file-manager,
+      # pkgs.cosmic-settings,
+      # pkgs.cosmic-wallpapers,
+      # pkgs.cosmic-control-center,
+      # pkgs.cosmic-notifications,
+      # pkgs.cosmic-calendar,
+      # pkgs.cosmic-clipboard,
+      # pkgs.cosmic-power-manager,
+      # pkgs.cosmic-session
     ];
   };
 }
