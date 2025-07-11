8780749f0296c9b8e09fef50d39bc861d846131a
hengvvang <hengvvang@proton.me>
2025-07-11 20:35:08 +0800

    use flatpak

--- pkgs/apps/browsers.nix
+++ pkgs/apps/browsers.nix
@@ -3,7 +3,7 @@
 {
   config = lib.mkIf config.myPkgs.apps.browsers.enable {
     environment.systemPackages = with pkgs; [
-      google-chrome   # Google Chrome 浏览器
+      # google-chrome   # Google Chrome 浏览器
     ];
   };
 }
