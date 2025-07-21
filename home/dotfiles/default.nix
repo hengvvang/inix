3943858c6d11c6799257dcd200ef7bbaacb8711c
hengvvang <hengvvang@gmail.com>
2025-07-22 00:16:25 +0800

    fix: remove non-existent mpd module from dotfiles imports

--- home/dotfiles/default.nix
+++ home/dotfiles/default.nix
@@ -22,7 +22,6 @@
     ./starship
     ./qutebrowser   # 新增 Qutebrowser 浏览器配置
     ./obs-studio    # 新增 OBS Studio 配置
-    ./mpd           # 新增 MPD 用户级音乐服务配置
     ./rmpc          # rmpc 终端音乐播放器配置
     ./proxy       # 代理配置模块
   ];
