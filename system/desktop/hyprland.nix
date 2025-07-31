0d57d164700cc9b63041df1ac807addc605fbd85
hengvvang <hengvvang@gmail.com>
2025-07-31 12:25:15 +0800

    hyprland

--- system/desktop/hyprland.nix
+++ system/desktop/hyprland.nix
@@ -6,7 +6,9 @@
 
         programs.hyprland = {
             enable = true;
+            xwayland.enable = true;
             withUWSM = true;
+            package = pkgs.hyprland;
         };
         environment.sessionVariables.NIXOS_OZONE_WL = "1";
         environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
@@ -23,6 +25,9 @@
             hyprpaper
 
             kitty
+            wofi
+            waybar
+            nwg-look
         ];
     };
 }
