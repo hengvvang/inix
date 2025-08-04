dcd752f8d2604c60f19470b14cd4a82dd9b681b5
hengvvang <hengvvang@gmail.com>
2025-08-04 11:28:39 +0800

    none

--- home/desktop/niri/external/ironbar/default.nix
+++ home/desktop/niri/external/ironbar/default.nix
@@ -18,38 +18,5 @@
       ironbar
     ];
 
-    # 可选：创建 ironbar 服务 (如果需要 systemd 服务管理)
-    systemd.user.services.ironbar = {
-      Unit = {
-        Description = "Ironbar - GTK status bar for Wayland";
-        PartOf = [ "graphical-session.target" ];
-        After = [ "graphical-session.target" ];
-        Requisite = [ "graphical-session.target" ];
-      };
-
-      Service = {
-        Type = "simple";
-        ExecStart = "${pkgs.ironbar}/bin/ironbar";
-        Restart = "on-failure";
-        RestartSec = 3;
-        Environment = [
-          "PATH=${lib.makeBinPath (with pkgs; [
-            glib
-            gtk3
-            pango
-            cairo
-            gdk-pixbuf
-            pulseaudio
-            networkmanager
-            upower
-          ])}"
-        ];
-      };
-
-      Install = {
-        WantedBy = [ "graphical-session.target" ];
-      };
-    };
-
   };
 }
