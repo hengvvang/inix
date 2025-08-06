5df9b440242cc35656feb3aad9b487139ddac9ad
hengvvang <hengvvang@gmail.com>
2025-08-06 12:12:26 +0800

    feat: 清理旧的dotfiles配置文件，完成vscode设计模式重构

--- home/dotfiles/yazi/direct/default.nix
+++ home/dotfiles/yazi/direct/default.nix
@@ -1,26 +1,26 @@
 { config, lib, pkgs, ... }:
 
-let
-  yaziConfig = import ./yazi-config.nix { inherit config lib pkgs; };
-in
 {
   config = lib.mkIf (config.myHome.dotfiles.enable && config.myHome.dotfiles.yazi.enable && config.myHome.dotfiles.yazi.method == "direct") {
+    home.packages = with pkgs; [
+      yazi
+      ffmpegthumbnailer
+      unar
+      jq
+      poppler
+      fd
+      ripgrep
+      fzf
+      zoxide
+    ];
+
+    home.file.".config/yazi/yazi.toml".text = ''
+      [manager]
+      show_hidden = false
+    '';
     
-    home.packages = with pkgs; [ yazi ];
-
-    home.file.".config/yazi/yazi.toml".text = yaziConfig.yaziToml;
-    home.file.".config/yazi/keymap.toml".text = yaziConfig.keymapToml;
-    home.file.".config/yazi/theme.toml".text = yaziConfig.themeToml;
-    home.file.".config/yazi/init.lua".text = yaziConfig.initLua;
-  };
-}
-    '';
-    home.file.".config/yazi/plugins/smart-entry.yazi/README.md".text = ''
-    '';
-
-    home.file.".config/yazi/flavors/tokyo-night.yazi/flavor.toml".text = ''
-    '';
-    home.file.".config/yazi/flavors/tokyo-night.yazi/tmtheme.xml".text = ''
+    home.file.".config/yazi/keymap.toml".text = ''
+      # Yazi keymap config
     '';
   };
 }
