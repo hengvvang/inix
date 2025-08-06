79a2842bfed4f51e452e98fbd5486f3d51810039
hengvvang <hengvvang@gmail.com>
2025-08-06 11:19:52 +0800

    refactor: reorganize zsh dotfiles following vscode pattern
    - Move homemanager.nix to homemanager/default.nix
    - Create direct method with separated zsh-config.nix
    - Setup external method referencing ../configs/ files
    - Add xdirect method using builtins.readFile
    - Update imports to use directory-based structure
    - All 4 methods: homemanager, direct, external, xdirect

--- home/dotfiles/zsh/default.nix
+++ home/dotfiles/zsh/default.nix
@@ -6,20 +6,22 @@
     
     # 配置方式选择
     method = lib.mkOption {
-      type = lib.types.enum [ "homemanager" "direct" "external" ];
+      type = lib.types.enum [ "homemanager" "direct" "external" "xdirect" ];
       default = "homemanager";
       description = ''
         Zsh 配置方式:
         - homemanager: 使用 Home Manager 程序模块 (推荐)
         - direct: 直接写入 zshrc 文件 (演示用)
         - external: 引用外部 zshrc 文件 (演示用)
+        - xdirect: 使用 builtins.readFile 读取外部文件 (演示用)
       '';
     };
   };
 
   imports = [
-    ./homemanager.nix
-    ./direct.nix
-    ./external.nix
+    ./homemanager
+    ./direct
+    ./external
+    ./xdirect
   ];
 }
---
+++ home/dotfiles/zsh/xdirect/default.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ... }:
+
+{
+  config = lib.mkIf (config.myHome.dotfiles.enable && config.myHome.dotfiles.zsh.enable && config.myHome.dotfiles.zsh.method == "xdirect") {
+    
+    home.packages = with pkgs; [ zsh ];
+    
+    # Zsh 官方配置文件结构
+    home.file.".config/zsh/.zshenv".text = builtins.readFile ../configs/.zshenv;
+    home.file.".config/zsh/.zprofile".text = builtins.readFile ../configs/.zprofile;
+    home.file.".config/zsh/.zshrc".text = builtins.readFile ../configs/.zshrc;
+    home.file.".config/zsh/.zlogin".text = builtins.readFile ../configs/.zlogin;
+    home.file.".config/zsh/.zlogout".text = builtins.readFile ../configs/.zlogout;
+  };
+}
